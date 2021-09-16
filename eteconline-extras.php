5f0aa0c7258f05952848d6aaffde975dc0f01d30
Michael Wender <michael@michaelwender.com>
2021-09-16 07:49:35 -0400

    Updating newsletters

--- eteconline-extras.php
+++ eteconline-extras.php
@@ -7,7 +7,7 @@
  * Author URI:      https://mwender.com
  * Text Domain:     eteconline-extras
  * Domain Path:     /languages
- * Version:         0.4.5
+ * Version:         0.4.6
  *
  * @package         Eteconline_Extras
  */
